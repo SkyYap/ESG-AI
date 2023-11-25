e7ead77aff3b1cde47f3adac33483f4011d0456e
Furqan Qadri <32431583+furqan-qadri@users.noreply.github.com>
2023-11-26 06:53:10 +0800

    add HLB suggestions in table

--- src/app/dashboard/action/page.tsx
+++ src/app/dashboard/action/page.tsx
@@ -1,9 +1,14 @@
 'use client'
-
+import Image from 'next/image';
 import { Separator } from '@/components/ui/Separator';
 import ActionHeader from './_PageSections/ActionHeader';
 import ActionText from './_PageSections/ActionText';
 import { Outline } from '../report/_PageSections/Outline';
+import HongTable from './_PageSections/HongTable';
+import HLB from './_PageSections/HLB';
+// import Hong from './hong_leong.png';
+
+import treeImage from './tree.png'; 
 
 export default async function PdfUploadPage() {
   return (
@@ -13,6 +18,9 @@ export default async function PdfUploadPage() {
         <Separator className="my-4" />
         <ActionText/>
         <Outline/>
+        <HLB/>
+        {/* <Image src={Hong} alt="kj" /> */}
+        <HongTable/>
 
 
     </div>
