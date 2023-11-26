529be56d4d60d649dfb7dc76bf5c9efe5cd1d096
Furqan Qadri <32431583+furqan-qadri@users.noreply.github.com>
2023-11-26 09:49:29 +0800

    add dummy actions

--- src/app/dashboard/report/_PageSections/ApiAnswer.tsx
+++ src/app/dashboard/report/_PageSections/ApiAnswer.tsx
@@ -5,7 +5,7 @@ const ApiAnswer = () => {
   const [answer, setAnswer] = useState('');
   const [loading, setLoading] = useState(false);
 
-  const apiUrl = 'https://3573-34-106-210-166.ngrok.io/api/ask_question';
+  const apiUrl = 'https://57b3-34-106-210-166.ngrok.io/api/ask_question';
 
   const handleQuestionChange = (e) => {
     setQuestion(e.target.value);
